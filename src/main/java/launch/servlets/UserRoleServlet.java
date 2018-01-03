06078f17f8be20bd8b551d1c609df21e8faae21d
Alexey <oleksii.donchak@gmail.com>
2018-01-03 11:43:49 +0200

    Added servlet and pages for user role.

---
+++ src/main/java/launch/servlets/UserRoleServlet.java
@@ -0,0 +1,26 @@
+package launch.servlets;
+
+import models.UserRole;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "UserRoleServlet",
+        urlPatterns = {"/user-role"}
+)
+public class UserRoleServlet extends ModelServlet<UserRole> {
+    @Override
+    protected Class<UserRole> clazz() {
+        return UserRole.class;
+    }
+
+    @Override
+    protected String singularName() {
+        return "userRole";
+    }
+
+    @Override
+    protected String pluralName() {
+        return "userRoles";
+    }
+}
